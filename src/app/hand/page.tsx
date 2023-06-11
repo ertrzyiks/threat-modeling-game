6895e81d8383c80d430f35664b67427dabce6a9b
ertrzyiks <mateusz.derks@gmail.com>
2023-06-11 17:30:13 +0200

    Add transition

--- src/app/hand/page.tsx
+++ src/app/hand/page.tsx
@@ -1,50 +1,95 @@
 'use client'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { flushSync } from 'react-dom'
 import { Card } from '~/components/Card/Card'
 import { getCardByCode } from '~/getCardByCode'
 import { useSearchParams } from 'next/navigation'
 
+const withViewTransition = (fn: () => void) => {
+  if (!document.startViewTransition) {
+    fn()
+    return
+  }
+
+  document.startViewTransition(() => {
+    flushSync(() => {
+      fn()
+    })
+  })
+}
+
 export default function Home() {
   const params = useSearchParams()
-  const codes = params.get('cards')
-  const cards = (codes ? codes.split(',') : []).map(code => getCardByCode(code))
+  const codes = params.get('cards') ?? ''
+  const cardCodes = codes.split(',')
 
-  const [usedCards, setUsedCards] = useState<string[]>([])
+  const [activeCardCodes, setActiveCardCodes] = useState<string[]>(cardCodes)
+  const [discardedCardCodes, setDiscardedCardCodes] = useState<string[]>([])
 
   const toggleCard = (code: string) => {
-    if (usedCards.includes(code)) {
-      setUsedCards(usedCards.filter(usedCode => usedCode !== code))
-    } else {
-      setUsedCards([...usedCards, code])
-    }
+    withViewTransition(() => {
+      if (discardedCardCodes.includes(code)) {
+        setDiscardedCardCodes(discardedCardCodes.filter(discardedCardCode => discardedCardCode !== code))
+        setActiveCardCodes([...activeCardCodes, code])
+      } else {
+        console.log({ activeCardCodes, discardedCardCodes, code })
+        setDiscardedCardCodes([...discardedCardCodes, code])
+        setActiveCardCodes(activeCardCodes.filter(activeCardCode => activeCardCode !== code))
+      }
+    });
   }
+
+  const activeCards = activeCardCodes.map(code => getCardByCode(code))
+  const discardedCards = discardedCardCodes.map(code => getCardByCode(code))
 
   return (
     <main>
       <div className="px-12">
         <h2 className="text-4xl my-4">Your cards</h2>
 
-
-
-        <div className="flex flex-wrap gap-4">
-          {cards.map(card => (
-            <div
-              key={card.code}
-              className={['cursor-pointer transition-opacity duration-300', usedCards.includes(card.code) ? 'opacity-70' : ''].join(' ')}
-              onClick={() => toggleCard(card.code)}
-            >
-              <Card
-                code={card.code}
-                group={card.group}
-                value={card.value}
-                description={card.description}
-                className={usedCards.includes(card.code) ? 'text-slate-600 opacity-40' : card.className}
-              />
+        <div className="card-decks">
+          <div>
+            <div className="flex flex-wrap gap-4">
+              {activeCards.map(card => (
+                <div
+                  key={card.code}
+                  className='cursor-pointer'
+                  onClick={() => toggleCard(card.code)}
+                >
+                  <Card
+                    code={card.code}
+                    group={card.group}
+                    value={card.value}
+                    description={card.description}
+                    className={card.className}
+                  />
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
 
-        <p className="mt-4">Click a card to discard it, click again to bring it back.</p>
+            <p className="mt-4">Click a card to discard it, click again to bring it back.</p>
+          </div>
+          <div>
+            <div className="flex flex-wrap gap-4 relative">
+              {discardedCards.map((card, index) => (
+                <div
+                  key={card.code}
+                  className='cursor-pointer bg-white absolute top-0'
+                  onClick={() => toggleCard(card.code)}
+                  style={{ top: `${index}rem`, left: `${index}rem` }}
+                >
+                  <Card
+                    code={card.code}
+                    group={card.group}
+                    value={card.value}
+                    description={card.description}
+                    className={card.className}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   )
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -10,26 +10,24 @@ function Card({ code, group, value, description, className }: Props) {
   const highValue = ['J', 'Q', 'K', 'A'].includes(value)
 
   return (
-    <div className={['transition-all duration-300 ease-out w-64 h-96 border-solid border-2 border-current relative', className, highValue ? 'bg-current' : 'bg-white'].join(' ')}>
+    <div className={['transition-all duration-300 ease-out w-64 h-96 border-solid border-2 border-current relative', className, `card-${code}`, highValue ? 'bg-current' : 'bg-white'].join(' ')}>
       <div className="flex py-3">
-        <div title='Card value' className={['p-3 w-12 text-center rounded-e-lg text-md mr-2', highValue ? 'bg-white' : 'bg-current'].join(' ')}>
+        <div className={['p-3 w-12 text-center rounded-e-lg text-md mr-2', highValue ? 'bg-white' : 'bg-current'].join(' ')}>
           <span className={highValue ? 'text-current' : 'text-white'}>{value}</span>
         </div>
         <span
-          title='Card group'
           className={['py-3 text-md', highValue ? 'text-white opacity-80' : 'text-current'].join(' ')}
         >
           {group}
         </span>
       </div>
 
-      <div title='Card description' className={['mt-2 px-4 text-md', highValue ? 'text-white' : 'text-black'].join(' ')}>
+      <div className={['mt-2 px-4 text-md', highValue ? 'text-white' : 'text-black'].join(' ')}>
         {description}
       </div>
 
       <span className={["absolute bottom-0 right-0 text-9xl opacity-30", highValue ? 'text-white' : 'text-current'].join(' ')}>{value}</span>
       <span
-        title='Card code'
         className='absolute bottom-1 left-1 text-xs bg-white text-slate-400 px-2 border border-slate-300'
       >
         {code}
